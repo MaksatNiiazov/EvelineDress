12237f4848d03e000bc009fd1db7bad37b38dcd4
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-05-08 15:23:49 +0600

    ini

--- core/settings.py
+++ core/settings.py
@@ -124,8 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "apps/static"),)
+
+
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
